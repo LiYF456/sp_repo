8960387cfcbe46d1a581bf4dfea56b6448cdbdf3
Administrator <Administrator@PC-20190326LARV>
2019-06-29 10:46:01 +0800

    5

---
+++ sp02-itemservice/src/main/java/com/tedu/sp02/So02ItemserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tedu.sp02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class So02ItemserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(So02ItemserviceApplication.class, args);
+	}
+
+}
